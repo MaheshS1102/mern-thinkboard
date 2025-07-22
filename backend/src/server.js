5a36260ac1d0bfd5f4599f06ff089374f1edf18c
Mahesh Salunke <maheshgsal76@gmail.com>
2025-07-23 02:13:18 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 // middleware
 if (process.env.NODE_ENV !== "production") {
@@ -32,13 +32,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     });
-// }
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
